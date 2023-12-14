645486c4c9a1000a6e1bf50639da495256196635
Santosh Passoubady <santoshpassoubady@gmail.com>
2023-12-13 22:41:10 -0500

    added tailwind.css + blueprint.js still very sketch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,23 @@
 import './App.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagicWandSparkles } from '@fortawesome/free-solid-svg-icons';
-import React, { useState } from 'react';
-import SearchBar from './Navbar';
+import React from 'react';
 import Navbar from './Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="flex">
+      <div className="mt-10">
         <Navbar />
-        <button><FontAwesomeIcon icon={faMagicWandSparkles} /> New Grimoire</button>
+
+        <button>
+           <FontAwesomeIcon icon={faMagicWandSparkles} /> New Grimoire
+        </button>
+
     </div>
+    </div>  
   );
 }
+
 
 export default App;
--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { InputGroup } from "@blueprintjs/core";
 
 function  Navbar() {
-    const [searchTerm, setSearchTerm] = useState('');
     return (
-        <div>
-            <h3>Grimoire</h3>
-            <input
-                type="text"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-            />
+        <div className="h-screen w-screen p-5 flex justify-center">
+            <div className="flex justify-center w-3/5">
+                <InputGroup
+                placeholder="Search an Object..."
+                fill={true}
+                round={false}
+                leftIcon="search"
+                type="search" 
+                className="!z-5"/>
+            </div>
         </div>
     );
 }
